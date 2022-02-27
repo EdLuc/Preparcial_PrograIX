f3b631994462eb8b4d8794766246e0647bccdcba
Lucas <edgarlucas20@gmail.com>
2022-02-26 21:13:49 -0600

    Preparcial

---
+++ DemoDapperAPI/Controllers/ClientesController.cs
@@ -0,0 +1,133 @@
+﻿using DemoDapperAPI.Models;
+using DemoDapperAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoDapperAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        // GET: api/<ClientesController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Cliente>> Get()
+        {
+            var clienteService = new ClientServices();
+            {
+                var cliente = clienteService.GetClientes();
+                if (cliente != null)
+                {
+                    return Ok(cliente);
+                }
+                return NotFound("Mensaje: There is not clients");
+            }
+        }
+
+        [HttpGet]
+        [Route("ASYNC")]
+        public async Task<ActionResult<IEnumerable<Cliente>>> GetAsync()
+        {
+            var clienteService = new ClientServices();
+            {
+                var cliente = await clienteService.GetClientesAsync();
+                if (cliente != null)
+                {
+                    return Ok(cliente);
+                }
+                return NotFound("Mensaje: There is not clients");
+            }
+        }
+
+        // GET api/<ClientesController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Cliente> Get(int id)
+        {
+            var clienteService = new ClientServices();
+            {
+                var cliente = clienteService.GetClienteById(id);
+                if (cliente != null)
+                {
+                    return Ok(cliente);
+                }
+                return NotFound("Mensaje: There is not client with id: " + id);
+            }
+        }
+
+        // POST api/<ClientesController>
+        [HttpPost]
+        public void Post([FromBody] Cliente cliente)
+        {
+            var clienteService = new ClientServices();
+            {
+                clienteService.AddClient(cliente, true);
+
+            }
+        }
+
+        //api/cliente/async
+        [HttpPost]
+        [Route("ASYNC")]
+        public async Task PostAsync([FromBody] Cliente cliente)
+        {
+            try
+            {
+                var clienteService = new ClientServices();
+                {
+                    cliente.id = 0;
+                    await clienteService.AddClientAsync(cliente);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        // PUT api/<ClientesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Cliente cliente)
+        {
+            try
+            {
+                var clienteService = new ClientServices();
+                {
+                    cliente.id = id;
+                    clienteService.UpdateClient(cliente, true);
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        [HttpPut("{id}")]
+        [Route("ASYNC")]
+        public async Task PutAsync([FromBody] Cliente cliente)
+        {
+            try
+            {
+                var clienteService = new ClientServices();
+                {
+                    await clienteService.UpdateClientAsync(cliente);
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        // DELETE api/<ClientesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DemoDapperAPI/Services/ClientServices.cs
@@ -0,0 +1,154 @@
+﻿using Dapper;
+using DapperExtensions;
+using DemoDapperAPI.DML;
+using DemoDapperAPI.Models;
+using System.Data.SqlClient;
+
+namespace DemoDapperAPI.Services
+{
+    public class ClientServices
+    {
+        private SqlConnection _Conn = new SqlConnection();
+        //Instalar Dapper y SQL 
+        private static SqlConnection GetSqlConnection()
+        {
+            return new SqlConnection(@"Data Sourse=localhost;Initial Catalog=ClienteIX; Integrated Security=True; Pooling=False");
+        }
+        public Cliente GetClienteById(int id)
+        {
+            _Conn = GetSqlConnection();
+            _Conn.Open();
+            //Select 
+            var cliente = _Conn.Query<Cliente>("SELECT * FROM Cliente").Where(f => f.id == id).ToList();
+            return cliente.Count != 0 ? cliente.First() : null;
+        }
+        public IEnumerable<Cliente> GetClientes()
+        {
+            _Conn = GetSqlConnection();
+            _Conn.Open();
+            //Select 
+            var clientes = _Conn.Query<Cliente>("SELECT * FROM Cliente").ToList();
+            return clientes;
+        }
+
+        public async Task<IEnumerable<Cliente>> GetClientesAsync()
+        {
+            _Conn = GetSqlConnection();
+            _Conn.Open();
+            //Select 
+            var clientes = await _Conn.QueryAsync<Cliente>("SELECT * FROM Cliente");
+            return clientes;
+        }
+
+        public void AddClient(Cliente cliente)
+        {
+            try
+            {
+                _Conn = GetSqlConnection();
+                _Conn.Open();
+                var strInsert = DMLGenerator.CreateInsertStatement(cliente);
+                var clientes = _Conn.Execute(strInsert, cliente);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public void AddClient(Cliente cliente, bool UseDapper = true)
+        {
+            try
+            {
+                _Conn = GetSqlConnection();
+                _Conn.Open();
+                if (UseDapper)
+                {
+                    _Conn.Insert(cliente);
+                    _Conn.Close();
+                }
+                else
+                {
+                    var strInsert = DMLGenerator.CreateInsertStatement(cliente);
+                    var clientes = _Conn.Execute(strInsert, cliente);
+                }
+                
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+        public async Task AddClientAsync(Cliente cliente, bool UseDapper = true)
+        {
+            try
+            {
+                _Conn = GetSqlConnection();
+                _Conn.Open();
+                if (UseDapper)
+                {
+                    await _Conn.InsertAsync(cliente);
+                    _Conn.Close();
+                }
+                else
+                {
+                    var strInsert = DMLGenerator.CreateInsertStatement(cliente);
+                    var clientes = _Conn.Execute(strInsert, cliente);
+                }
+                
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+        public void UpdateClient(Cliente cliente, bool UseDapper = true, bool UseEntity = false)
+        {
+            try
+            {
+                _Conn = GetSqlConnection();
+                _Conn.Open();
+                if (UseDapper)
+                {
+                    _Conn.Update(cliente);
+                    _Conn.Close();
+                }
+                else
+                {
+                    //TODO
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        public async Task UpdateClientAsync(Cliente cliente, bool UseDapper = true)
+        {
+            try
+            {
+                _Conn = GetSqlConnection();
+                _Conn.Open(); 
+                if (UseDapper)
+                {
+                    await _Conn.UpdateAsync(cliente);
+                    _Conn.Close();
+                }
+                else
+                {
+                    var strInsert = DMLGenerator.CreateUpdateStatement(cliente);
+                    var clientes = _Conn.Execute(strInsert, cliente);
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+    }
+}
